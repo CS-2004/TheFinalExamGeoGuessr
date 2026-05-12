a26ae4d960eedc61b79556d4d45fceaca69477f2
PS <jerman.loera01@utrgv.edu>
2026-05-12 10:32:09 -0500

    added template files to begin implementation

---
+++ views.py
@@ -0,0 +1,12 @@
+questions = [
+    {"country": "France", "capital": "Paris"},
+    {"country": "Canada", "capital": "Montreal"},
+    {"country": "Brazil", "capital": "Brasilia"},
+    {"country": "Monaco", "capital": "Monaco"},
+    {"country": "Spain", "capital": "Madrid"},
+    {"country": "Singapore", "capital": "Singapore"},
+    {"country": "Japan", "capital": "Tokio"},
+    {"country": "Israel", "capital": "Jerusalem"},
+    {"country": "China", "capital": "Beijing"},
+    {"country": "Australia", "capital": "Canberra"},
+]
